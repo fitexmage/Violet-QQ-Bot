71e2e1891ebc351a3192931c98c93985b25862b8
Shengguang Bai <fitexmage@126.com>
2019-03-26 15:44:14 -0400

    Fix a bug which will make the reply be nothing.

--- crawler.py
+++ crawler.py
@@ -60,6 +60,7 @@ def search_url(message):
     for word in list:
         if word not in stopwords_set:
             new_list.append(word)
+
     result_list = []
     with open(new_url_path) as f:
         reader = csv.reader(f)
@@ -70,7 +71,7 @@ def search_url(message):
                     if word in row[2]:
                         count += 1
 
-                if count >= len(new_list) / 2:
+                if count >= len(new_list) * 0.7:
                     result_list.append(row)
 
     if len(result_list) > 0:
@@ -83,15 +84,17 @@ def crawler_result(url):
     driver.get(url)
     time.sleep(1)
     post_list = driver.find_element_by_id('postlist').find_elements_by_class_name('plhin')
+
     if len(post_list) > 1:
         author = post_list[0].find_element_by_class_name('authi').text
-        for i in range(3):
+
+        for i in range(5):
             r = random.randint(1, len(post_list)-1)
             post = post_list[r]
             if author is not post.find_element_by_class_name('authi').text:
                 reply = post.find_element_by_class_name('t_f').text
                 reply = reply.split("\n")[-1]
-                if re.match('http.*', reply) is not None:
+                if re.match('http.*', reply) is None:
                     return reply
 
     return "对不起，我不太懂，我还需要学习~"
