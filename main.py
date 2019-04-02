98379962eccccf78a914391de913a77d800e9e71
Shengguang Bai <fitexmage@126.com>
2019-04-01 20:54:02 -0400

    Remove whitelist when someone quit group chat.

--- main.py
+++ main.py
@@ -39,7 +39,12 @@ async def handle_group_increase(context):
 
 @bot.on_notice('group_decrease')
 async def handle_group_decrease(context):
-    print(context)
+    if context['group_id'] == 298466962:
+        qq_number = context['user_id']
+        if qq_number in violet.player_qq_dict:
+            violet.rcon_command("whitelist remove " + violet.player_qq_dict[qq_number])
+            violet.player_qq_dict.pop(qq_number)
+            violet.update_qq_dict()
 
 
 @bot.on_request('group')
