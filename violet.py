db07329fc682a99b517aaa45072ca9ebf25dab68
Shengguang Bai <fitexmage@126.com>
2020-02-18 15:57:45 +0800

    Debug

--- violet.py
+++ violet.py
@@ -120,7 +120,8 @@ class Violet:
             else:
                 self_qq = str(context['user_id'])
                 opponent_qq = par_list[1]
-                info = bot.get_group_member_info(group_id=str(context['group_id']), user_id=opponent_qq)
+                info = bot.get_group_member_info(group_id=context['group_id'], user_id=int(opponent_qq))
+                print(info)
                 print(info['group_id'])
 
 
